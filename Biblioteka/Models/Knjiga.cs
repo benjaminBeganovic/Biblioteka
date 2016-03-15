6d2cdea4ca1e274be0a2b403c7121647d8d7196e
benjaminBeganovic <benjamin.beganovic33@gmail.com>
2016-03-16 00:18:21 +0100

    Dodane klase

---
+++ Biblioteka/Models/Knjiga.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biblioteka.Models
+{
+    public class Knjiga
+    {
+        public long KnjigaId { get; set; }
+        [Key]
+        [ForeignKey("Izdavac")]
+        public long IzdavacID { get; set; }
+        [Key]
+        [ForeignKey("TipKnjige")]
+        public long TipKnjigeID { get; set; }
+        [Key]
+        [ForeignKey("Jezik")]
+        public long JezikID { get; set; }
+    }
+}
