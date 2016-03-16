e22409cb502d4e16075bdf0e01217a405d25a5f2
akadrispahic <akadrispah1@etf.unsa.ba>
2016-03-16 09:10:32 +0100

    Autor /KnjigaAutor / TipRacuna
    Mapiranje Autor /KnjigaAutor / TipRacuna

--- Biblioteka/Models/KnjigaAutor.cs
+++ Biblioteka/Models/KnjigaAutor.cs
@@ -7,5 +7,14 @@ namespace Biblioteka.Models
 {
     public class KnjigaAutor
     {
+        [Key]
+        [ForeignKey("Knjiga")]
+        public long KnjigaID { get; set; }
+        [Key]
+        [ForeignKey("Autor")]
+        public long AutorID { get; set; }
+
+        public virtual Knjiga Knjiga { get; set; }
+        public virtual Autor Autor { get; set; }
     }
 }
--- Biblioteka/Models/TipRacuna.cs
+++ Biblioteka/Models/TipRacuna.cs
@@ -8,7 +8,6 @@ namespace Biblioteka.Models
     public class TipRacuna
     {
         public long ID { get; set; }
-        [Key]
         public string referenca { get; set; }
         public string opis { get; set; }
        
