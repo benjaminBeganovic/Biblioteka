b8b1ce6235efbcf81e14f7c11beb237d1b0ed783
mirhat <babic.mirhat@gmail.com>
2016-03-26 19:06:27 +0100

    Dodan login i logout u insomniji

--- Biblioteka/Controllers/AuthController.cs
+++ Biblioteka/Controllers/AuthController.cs
@@ -13,6 +13,8 @@ namespace Biblioteka.Controllers
     public class AuthController : ApiController
     {
         private ProbaContext db = new ProbaContext();
+
+        [ActionName("Login")]
         [System.Web.Mvc.HttpPost]
         public IHttpActionResult Login(string username, string password)
         {
@@ -30,6 +32,27 @@ namespace Biblioteka.Controllers
                 return NotFound();
             }
             SessionPersister.username = k.username;
+            return Ok();
+        }
+
+        [ActionName("Logout")]
+        [System.Web.Mvc.HttpPost]
+        public IHttpActionResult Logout(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest(ModelState);
+            }
+            Korisnik k;
+            if (db.Korisniks.Any(a => a.username == username && a.password == password))
+            {
+                k = db.Korisniks.Where(a => a.username == username && a.password == password).First();
+            }
+            else
+            {
+                return NotFound();
+            }
+            SessionPersister.username = null;
             return Ok();
         }
     }
