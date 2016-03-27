a068b907670b43078eaaa30a932316fdbd584f49
mirhat <babic.mirhat@gmail.com>
2016-03-27 14:44:42 +0200

    Zavrsena pretraga kontroler i json

--- Biblioteka/Controllers/PretragaController.cs
+++ Biblioteka/Controllers/PretragaController.cs
@@ -13,21 +13,106 @@ namespace Biblioteka.Controllers
     {
         private ProbaContext db = new ProbaContext();
 
-        [ActionName("Autori")]
-        [ResponseType(typeof(List<Knjiga>))]
-        public IHttpActionResult GetAutori(string naziv, string autor)
+        void prepareValue(ref string a)
         {
-            List<Knjiga> knjiga = null;
-            knjiga = db.Knjigas.Where(a => a.naslov.ToLower().Contains(naziv.ToLower())).ToList();
-            List<Knjiga> rez = new List<Knjiga>();
-            foreach (var k in knjiga)
+            if (a != null)
             {
-                if (k.Autori.Any(a => a.naziv.ToLower().Contains(autor)))
+                a = a.ToLower().Trim();
+            }
+        }
+
+        [ActionName("Kod")]
+        [ResponseType(typeof(List<Knjiga>))]
+        public IHttpActionResult GetPretragaKod(string kod)
+        {
+            prepareValue(ref kod);
+            if (kod != null)
+            {
+                var knjige = db.Knjigas.Where(a => a.idbroj.ToLower().Contains(kod)).ToList();
+                if (knjige.Count() > 0)
                 {
-                    rez.Add(k);
+                    return Ok(knjige);
+                }
+                return NotFound();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [ActionName("Napredna")]
+        [ResponseType(typeof(List<Knjiga>))]
+        public IHttpActionResult GetPretragaNapredna(string naziv, string tip, string jezik, string autor, string izdavac, int godina, List<string> kljucne)
+        {
+            prepareValue(ref naziv);
+            prepareValue(ref autor);
+            prepareValue(ref izdavac);
+            prepareValue(ref tip);
+            prepareValue(ref jezik);
+            List<Knjiga> knjige = null;
+            IQueryable<Knjiga> knjiga = null;
+            if (autor != null && naziv != null)
+            {
+                knjiga = db.Knjigas.Where(a => a.naslov.ToLower().Contains(naziv))
+                .Where(t => t.TipKnjige.referenca == tip)
+                .Where(j => j.Jezik.referenca.ToLower() == jezik.ToLower());
+                var rez = new List<Knjiga>();
+                foreach (var k in knjiga.ToList())
+                {
+                    if (k.Autori.Any(a => a.naziv.ToLower().Contains(autor)))
+                    {
+                        rez.Add(k);
+                    }
+                }
+                knjiga = rez.AsQueryable();
+            }
+            else if (naziv != null)
+            {
+                knjiga = db.Knjigas.Where(a => a.naslov.ToLower().Contains(naziv))
+                .Where(t => t.TipKnjige.referenca == tip)
+                .Where(j => j.Jezik.referenca.ToLower() == jezik.ToLower());
+            }
+            else if (autor != null)
+            {
+                knjiga = db.Knjigas.Where(t => t.TipKnjige.referenca == tip)
+                .Where(j => j.Jezik.referenca.ToLower() == jezik.ToLower());
+                var rez = new List<Knjiga>();
+                foreach (var k in knjiga.ToList())
+                {
+                    if (k.Autori.Any(a => a.naziv.ToLower().Contains(autor)))
+                    {
+                        rez.Add(k);
+                    }
+                }
+                knjiga = rez.AsQueryable();
+            }
+            if (izdavac != null)
+            {
+                knjiga = knjiga.Where(a => a.Izdavac.naziv.ToLower().Contains(izdavac));
+            }
+            if (godina != 0)
+            {
+                knjiga = knjiga.Where(a => a.godina_izdavanja.Value.Year == godina);
+            }
+            if (kljucne != null)
+            {
+                foreach (var k in kljucne)
+                {
+                    if (knjiga.Any(a => a.opis.ToLower().Contains(k)))
+                    {
+                        knjiga = knjiga.Where(a => a.opis.ToLower().Contains(k));
+                    }
                 }
             }
-            return Ok(rez);
+            if (knjiga.ToList().Count() == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(knjiga.ToList());
+            }
         }
 
         [AllowAnonymous]
