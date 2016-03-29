b922560cbe7ff41f0cbc91ca27f34a1bdea38f64
akadrisphaic <akadrispah1@etf.unsa.ba>
2016-03-29 12:46:37 +0200

    Informacije Model i contoller
    Informacije-

---
+++ Biblioteka/Controllers/InformacijesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Biblioteka.Models;
+
+namespace Biblioteka.Controllers
+{
+    public class InformacijesController : ApiController
+    {
+        private ProbaContext db = new ProbaContext();
+
+        // GET: api/Informacijes
+        public IQueryable<Informacije> GetInformacijes()
+        {
+            return db.Informacijes;
+        }
+
+        // GET: api/Informacijes/5
+        [ResponseType(typeof(Informacije))]
+        public IHttpActionResult GetInformacije(long id)
+        {
+            Informacije informacije = db.Informacijes.Find(id);
+            if (informacije == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(informacije);
+        }
+
+        // PUT: api/Informacijes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutInformacije(long id, Informacije informacije)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != informacije.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(informacije).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InformacijeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Informacijes
+        [ResponseType(typeof(Informacije))]
+        public IHttpActionResult PostInformacije(Informacije informacije)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Informacijes.Add(informacije);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = informacije.ID }, informacije);
+        }
+
+        // DELETE: api/Informacijes/5
+        [ResponseType(typeof(Informacije))]
+        public IHttpActionResult DeleteInformacije(long id)
+        {
+            Informacije informacije = db.Informacijes.Find(id);
+            if (informacije == null)
+            {
+                return NotFound();
+            }
+
+            db.Informacijes.Remove(informacije);
+            db.SaveChanges();
+
+            return Ok(informacije);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool InformacijeExists(long id)
+        {
+            return db.Informacijes.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ Biblioteka/Models/Informacije.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteka.Models
+{
+    public class Informacije
+    {
+        [ScaffoldColumn(false)]
+        [Display(Name ="ID")]
+        public long ID { get; set; }
+
+        [Required]
+        [Display(Name = "Key")]
+        public string key { get; set; }
+    
+        [Required]
+        [Display(Name ="Value")]
+        public string value { get; set; }
+
+        [Required]
+        [Display(Name = "Binary")]
+        public Byte [] binary { get; set; }
+
+
+    }
+}
