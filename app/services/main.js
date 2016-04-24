8f5f3ea7c1616e9e52a834a1291890dc029915c0
mirhat <babic.mirhat@gmail.com>
2016-04-24 13:52:17 +0200

    Skoro svi apiji u servisu main.js

--- app/services/main.js
+++ app/services/main.js
@@ -1,13 +1,12 @@
 angular.module('BibliotekaApp').factory("BibliotekaService", ['$http', function($http) {
     
     var serviceBase = 'http://nwtbiblioteka.azurewebsites.net/';
-    //$http.defaults.headers.post["Content-Type"] =
-      //  "application/x-www-form-urlencoded; charset=UTF-8;";
+
     return {
         login: function(loginModel)
         {
             return $http({
-                url: serviceBase + '/api/Auth/Login',
+                url: serviceBase + 'api/Auth/Login',
                 method: "POST",
                 data: JSON.stringify(loginModel),
                 withCredentials: true
@@ -15,22 +14,119 @@ angular.module('BibliotekaApp').factory("BibliotekaService", ['$http', function(
         },
         logout: function () {
             return $http({
-                url: serviceBase + '/api/Auth/Logout',
+                url: serviceBase + 'api/Auth/Logout',
                 method: "POST",
                 withCredentials: true
             });
         },
-        register: function(registerModel)
-                    {
-            return $http.post(serviceBase + 'api/Auth/Register', registerModel);
+        register: function(korisnikModel)
+        {
+            return $http({
+                url: serviceBase + 'api/Korisniks',
+                method: "POST",
+                data: JSON.stringify(korisnikModel),
+                withCredentials: true
+            });
         },
         clanstva: function()
         {
             return $http({
-                url: serviceBase + '/api/Clanstvoes/',
+                url: serviceBase + 'api/Clanstvoes',
                 method: "GET",
                 withCredentials: true
             });
         },
+        svitipoviknjiga: function () {
+            return $http({
+                url: serviceBase + 'api/TipKnjiges',
+                method: "GET",
+                withCredentials: true
+            });
+        },
+        naprednapretraga: function (pretragaModel) {
+            return $http({
+                url: serviceBase + 'api/Pretraga/Napredna',
+                method: "GET",
+                data: JSON.stringify(pretragaModel),
+                withCredentials: true
+            });
+        },
+        jednostavnapretraga: function (pretragaModel) {
+            return $http({
+                url: serviceBase + 'api/Pretraga/Jednostavna',
+                method: "GET",
+                data: JSON.stringify(pretragaModel),
+                withCredentials: true
+            });
+        },
+        pretragakod: function (kod) {
+            return $http({
+                url: serviceBase + 'api/Pretraga/Kod',
+                method: "GET",
+                data: JSON.stringify(kod),
+                withCredentials: true
+            });
+        },
+        dajkorisnika: function (id) {
+            return $http({
+                url: serviceBase + 'api/Korisniks/' + id,
+                method: "GET",
+                withCredentials: true
+            });
+        },
+        dajsvekorisnike: function () {
+            return $http({
+                url: serviceBase + 'api/Korisniks',
+                method: "GET",
+                withCredentials: true
+            });
+        },
+        obrisikorisnika: function (id) {
+            return $http({
+                url: serviceBase + 'api/Korisniks/' + id,
+                method: "DELETE",
+                withCredentials: true
+            });
+        },
+        izmijenikorisnika: function (korisnikModel) {
+            return $http({
+                url: serviceBase + 'api/Korisniks/' + korisnikModel.id,
+                method: "PUT",
+                data: JSON.stringify(korisnikModel),
+                withCredentials: true
+            });
+        },
+        zaduzi: function (zaduzenjeModel) {
+            return $http({
+                url: serviceBase + 'api/Zaduzenja',
+                method: "POST",
+                data: JSON.stringify(zaduzenjeModel),
+                withCredentials: true
+            });
+        },
+        izmijenizaduzenje: function (zaduzenjeModel) {
+            return $http({
+                url: serviceBase + 'api/Zaduzenja/' + zaduzenjeModel.id,
+                method: "PUT",
+                data: JSON.stringify(zaduzenjeModel),
+                withCredentials: true
+            });
+        },
+        napraviclanstvo: function (clanstvoModel) {
+            return $http({
+                url: serviceBase + 'api/Clanstvoes',
+                method: "POST",
+                data: JSON.stringify(clanstvoModel),
+                withCredentials: true
+            });
+        },
+        rezervisi: function (rezervacijaModel) {
+            return $http({
+                url: serviceBase + 'api/Rezervacija',
+                method: "POST",
+                data: JSON.stringify(rezervacijaModel),
+                withCredentials: true
+            });
+        }
 	};
 }]);
